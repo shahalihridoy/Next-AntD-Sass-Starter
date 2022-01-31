dfe10628db125ed093b2e14cbd985bd658080353
Hridoy <shahalihridoy@gmail.com>
2022-01-31 19:57:12 +0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withLess = require('next-with-less')
+
+const nextConfig = withLess({
   reactStrictMode: true,
-}
+  swcMinify: true,
+  lessLoaderOptions: {
+    lessOptions: {
+      modifyVars: {
+        'primary-color': '#9900FF',
+        'border-radius-base': '4px'
+      }
+    }
+  }
+})
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import CustomButton from '@core/CustomButton'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -24,7 +25,7 @@ const Home: NextPage = () => {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <CustomButton type="primary">Documentation</CustomButton>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
